c9f06080069941129eee0ced6d8185d3fe6da554
esotericl1sa <doe336055@gmail.com>
2024-09-17 22:00:45 +0200

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -3,8 +3,8 @@ from random import sample
 
 def get_numbers_ticket(min_int, max_int, quantity):
     # Check for invalid arguments
-    if min_int > max_int or quantity <= 0 or quantity > (max_int - min_int + 1):
+    if min_int < 1 or max_int > 1000 or min_int > max_int or quantity <= 0 or quantity > (max_int - min_int + 1):
         return []
 
     # Return a list of unique random integers using sample() method
-    return sample(range(min_int, max_int + 1), quantity)
+    return sorted(sample(range(min_int, max_int + 1), quantity))
